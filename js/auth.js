0629498ef973ba33fa52d20041a6447423bcc302
Jiii-Eun <mh030231@gmail.com>
2025-06-10 14:48:39 +0900

    refactor: 코드 위치 변경

--- js/auth.js
+++ js/auth.js
@@ -2,6 +2,7 @@
 const inputs = document.querySelectorAll('.input_group');
 const pw = document.querySelector('#user_password');
 const pwCheck = document.querySelector('#user_password_check');
+const pwCheckError = pwCheck.closest('.input_warp').querySelector('.error_text');
 
 const form = document.querySelector('#authForm');
 const authBtn = document.querySelector('.auth.btn');
@@ -50,7 +51,6 @@ inputs.forEach(group =>{
       if(values.length < 8){
         errorText.textContent = '비밀번호를 8자 이상 입력해주세요.';
       }else if(values !== pwCheck.value){
-        const pwCheckError = pwCheck.closest('.input_warp').querySelector('.error_text');
         pwCheckError.textContent = '비밀번호가 일치하지 않습니다..';
       }else{
         clearError(input, errorText);
@@ -71,7 +71,6 @@ inputs.forEach(group =>{
 
 //submit버튼 활성화
 function checkFormClick(){
-
   let isClick = true;
 
   inputs.forEach(group =>{
@@ -81,7 +80,6 @@ function checkFormClick(){
       isClick = false;
     }
   });
-
   authBtn.disabled = !isClick;
   authBtn.style.backgroundColor = isClick ? 'var(--primary-100)' : 'var(--gray-400)';
   authBtn.style.cursor = isClick ? 'pointer' : 'unset';
