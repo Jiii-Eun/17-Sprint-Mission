6cc4adb111c41c6b3f6d7a665031c10359b64065
geha <ospac111@gmail.com>
2025-06-27 14:33:38 +0900

    feat: react-router-dom 설치후 main.jsx에서 routes 세팅 완료

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout.jsx";
+import Items from "./pages/Items.jsx";
+import AddItems from "./pages/AddItems.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Home from "./pages/Home.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="items" element={<Items />} />
+          <Route path="additem" element={<AddItems />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
